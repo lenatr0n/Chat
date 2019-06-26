840516855b69ee37843d347f45b7eaa9aa729122
System Administrator <root@D-10-19-48-47.dhcp4.washington.edu>
2019-06-26 12:49:32 -0700

    this is my first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import coolpic from './puppy.png'
+import TextInput from './TextInput'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="header">
+        <img src={coolpic} className="logo" alt="logo" />
+        chat
+      </header>
+      <TextInput/>
+    </div>
+  );
+}
+
+export default App;
