a7be4baa90b59007e8a350c628c446df713ae472
System Administrator <root@D-10-19-189-67.dhcp4.washington.edu>
2019-07-03 12:43:11 -0700

    started NamePicker.js

--- src/App.js
+++ src/App.js
@@ -2,17 +2,42 @@ import React from 'react';
 import './App.css';
 import coolpic from './puppy.png'
 import TextInput from './TextInput'
+import NamePicker from './NamePicker.js'
 
-function App() {
+class App extends React.Component { 
+
+ state = {
+    messages:[],
+    name:'',
+    editName:false,
+  } 
+
+  gotMessage =(text) => {
+    var newMessagesArray =[...this.state.messages,text]
+    this.setState({messages: newMessagesArray})
+  }
+
+
+render() {
+  var {messages} = this.state
   return (
     <div className="App">
       <header className="header">
         <img src={coolpic} className="logo" alt="logo" />
-              chat
+        chat
       </header>
-      <TextInput/>
+      <main className="messages">
+       {messages.map((m,i)=>{
+          return (<div key={i} className="bubble-wrap">
+            <div className="bubble">
+              <span>{m}</span>
+            </div>
+          </div>)
+       })}
+      </main>
+      <TextInput sendMessage={this.gotMessage} />
     </div>
-  );
+  )
 }
-
+}
 export default App;
