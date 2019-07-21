b658c163b2ac6833281165d13b41149d06d46141
System Administrator <root@Lenas-MBP.domain>
2019-07-21 01:53:41 -0700

    delete messages functionality

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import * as firebase from "firebase/app";
 import "firebase/firestore";
 import "firebase/storage";
 import Camera from 'react-snap-pic';
+import Div100vh from 'react-div-100vh'
 
 class App extends React.Component { 
 
@@ -24,7 +25,6 @@ class App extends React.Component {
     if(name){
       this.setState({name})
     }
-
     /* <=========================> */
     firebase.initializeApp({
       apiKey: "AIzaSyBkpfykf6-EJiOVNd6VCTKy7_HEKV5EBI8",
@@ -34,29 +34,42 @@ class App extends React.Component {
     });
     
     this.db = firebase.firestore();
-
     this.db.collection("messages").onSnapshot((snapshot) => {
       snapshot.docChanges().forEach((change) => {
         if (change.type === "added") {
           //console.log(change.doc.data())
-          this.receive(change.doc.data())
+          this.receive({
+            ...change.doc.data(),
+            id: change.doc.id
+          })
+        }
+        if(change.type ==='removed'){
+          this.remove(change.doc.id)
         }
       })
     })
     /* <=========================> */
   }
 
-
+remove = (id) => {
+var msgs = [...this.state.messages]
+var messages = msgs.filter(m=> m.id!==id)
+this.setState({messages})
+}
   receive = (m) => {
     const messages = [m, ...this.state.messages]
     messages.sort((a,b)=>b.ts-a.ts)
     this.setState({messages})
   }
 
-  takePicture = (img) => {
-    console.log(img)
+  takePicture = async (img) => {
     this.setState({showCamera:false})
-}
+    const imgID = Math.random().toString(36).substring(7);
+    var storageRef = firebase.storage().ref();
+    var ref = storageRef.child(imgID+'.jpg');
+    await ref.putString(img, 'data_url')
+    this.send({img: imgID})
+  }
 
   send = (m) => {
     this.db.collection("messages").add({
@@ -84,7 +97,7 @@ setEditName = (editName) => {
 render() {
   var {messages,name,editName} = this.state
   return (
-    <div className="App">
+    <Div100vh className="App">
       <header className="header">
         <div>
         <img src={coolpic} className="logo" alt="logo" />
@@ -99,7 +112,13 @@ render() {
       </header>
       <main className="messages">
        {messages.map((m,i)=>{
-          return <Message key ={i} m={m} name ={name}/>
+          return (<Message key ={i} m={m} name ={name}
+          onClick ={()=> {
+            if (name===m.from) {
+            this.db.collection ('message').doc(m.id).delete()
+            }
+          }}
+          />)
        })}
       </main>
       <TextInput sendMessage={text=> this.send({text})} 
@@ -107,18 +126,24 @@ render() {
 
 />
 {this.state.showCamera && <Camera takePicture={this.takePicture} />}
-    </div>
+    </Div100vh>
   )
 }
 }
 export default App;
 
+const bucket = 'https://firebasestorage.googleapis.com/v0/b/chat-fc2cc.appspot.com/o/'
+const suffix = '.jpg?alt=media'
+
 function Message(props){
-  var{m, name} = props
+  var{m, name,onClick} = props
     return(<div className="bubble-wrap" 
-  from={m.from=== name ? "me" : "you"}>
+  from={m.from=== name ? "me" : "you"}
+  onClick={onClick}
+  >
     <div className="bubble">
       <span>{m.text}</span>
+      {m.img &&<img alt="pic" src = {bucket+m.img+suffix} />}
     </div>
     {m.from!==name && <div className ="bubble-name">
       {m.from}
